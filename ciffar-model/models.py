d77cc5f557409dac45ff1ff86f2fb6a826355604
Mamadou CAMARA <mamadouc@aid.fr>
2024-01-18 16:48:49 +0100

    cifar10 modle

--- ciffar-model/models.py
+++ ciffar-model/models.py
@@ -1,6 +1,6 @@
 
-
 from torch import Tensor, nn
+import torch.nn.functional as F
 
 
 class SimpleModel(nn.Module):
@@ -22,3 +22,16 @@ class CIFAR10Model(nn.Module):
     """
     Hint: It is going to be very similar to SimpleModel
     """
+    def __init__(self, backbone: nn.Module, adapter: nn.Module, head: nn.Module) -> None:
+        super().__init__()
+        super().__init__()
+        self.backbone = backbone
+        self.adapter = adapter
+        self.head = head
+
+    def forward(self, x):
+        x = nn.MaxPool2d(2, 2)(F.relu(self.backbone(x)))
+        #x = self.backbone(x)
+        x = self.adapter(x)
+        x = self.head(x)
+        return x
--- ciffar-model/tasks.py
+++ ciffar-model/tasks.py
@@ -59,3 +59,39 @@ class CIFAR10Classification(TrainingTask):
     """
     Hint: It is going to be very similar to MNISTClassification
     """
+    def __init__(self, model: nn.Module, optimizer: PARTIAL_OPTIMIZER_TYPE, loss_function: nn.Module) -> None:
+        super().__init__(optimizer)
+
+        self.model = model
+        self.loss_function = loss_function
+
+        nrof_classes = 10
+        self.train_accuracy = Accuracy(task="multiclass", num_classes=nrof_classes)
+        self.validaiton_accuracy = Accuracy(task="multiclass", num_classes=nrof_classes)
+        self.test_accuracy = Accuracy(task="multiclass", num_classes=nrof_classes)
+
+    def forward(self, x) -> Tensor:
+        return self.model(x)
+
+    def training_step(self, batch, _) -> Tensor:
+        images, labels = batch
+        logits = self(images)
+        loss = self.loss_function(logits, labels)
+        self.train_accuracy(logits, labels)
+        self.log("train_loss", loss, on_step=True, on_epoch=True)
+        self.log("train_accuracy", self.train_accuracy, on_step=False, on_epoch=True)
+        return loss
+
+    def validation_step(self, batch, _):
+        images, labels = batch
+        preds = self(images)
+        self.validaiton_accuracy(preds, labels)
+        self.log("validation_accuracy", self.validaiton_accuracy, on_step=False, on_epoch=True)
+
+    def test_step(self, batch, _):
+        images, labels = batch
+        preds = self(images)
+        self.test_accuracy(preds, labels)
+        self.log("test_accuracy", self.test_accuracy, on_step=False, on_epoch=True)
+
+
